729aeb06de598521c2f5304bc98b9374a323d37b
MOT EL <mot_edu@outlook.com>
2021-09-16 20:33:24 +0700

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Browsershot\Browsershot;
 
 class HomeController extends Controller
 {
-    //
+    public function pdf(){
+        dd("Hello");
+        Browsershot::html("<div style='color: red'>Hello</div>")
+            ->save("save_me.png");
+    }
 }
