78a0935403bc9a9918df97c377873269f4969885
MOT EL <mot_edu@outlook.com>
2021-09-16 20:34:39 +0700

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,6 @@ use Spatie\Browsershot\Browsershot;
 class HomeController extends Controller
 {
     public function pdf(){
-        dd("Hello");
         Browsershot::html("<div style='color: red'>Hello</div>")
             ->save("save_me.png");
     }
