9029058f1c6eed26b766afc65b0933d7b4726d8e
MOT EL <mot_edu@outlook.com>
2021-09-17 16:04:01 +0700

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Spatie\Browsershot\Browsershot;
+use Mpdf\Config\ConfigVariables;
+use Mpdf\Config\FontVariables;
+use Mpdf\Mpdf;
 
 class HomeController extends Controller
 {
     public function pdf(){
-        Browsershot::html("<div style='color: red'>Hello</div>")
-            ->save("save_me.png");
+
+$html = "<div></div>";
+        $defaultConfig = (new ConfigVariables())->getDefaults();
+        $fontDirs = $defaultConfig['fontDir'];
+
+        $defaultFontConfig = (new FontVariables())->getDefaults();
+        $fontData = $defaultFontConfig['fontdata'];
+        $mpdf = new Mpdf([
+            "format" =>[148,210],
+            'fontDir' => array_merge($fontDirs, [
+                storage_path("fonts")
+            ]),
+            'fontdata' => $fontData + [
+                    'KhmerOS' => [
+                        'R' => 'KhmerOS.ttf',
+                        'I' => 'KhmerOS.ttf',
+                    ]
+                ],
+            'default_font' => 'KhmerOS',
+            'margin_top' => 0,
+            'margin_left' => 0,
+            'margin_right' => 0,
+            'margin_bottom' => 0,
+            "orientation" => "P"
+        ]);
+        $mpdf->WriteHTML($html);
+        $mpdf->Output(storage_path("app/receipts/receipt1.pdf"));
     }
 }
---
+++ resources/views/recep.blade.php
@@ -0,0 +1,282 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <style media="print">
+        .print.a5{
+            width: 14.8cm;
+            height: 21cm;
+            margin: 0 auto;
+            background-color: white;
+        }
+        .print.a5 .margin{
+            width: 12.8cm;
+            height: 19cm;
+            padding: 1cm;
+        }
+        .color.pen{color: #3C54A5;}
+        .color.red{color: red;}
+        .print.a5 .mmiddle{
+            width: 5.8cm;
+            text-align: center;
+        }
+        .print.a5 .mleft,
+        .print.a5 .mright
+        {
+            width: 3.5cm;
+        }
+        .print.a5 .sec2:nth-child(2){
+            width: 3cm;
+        }
+        .print.a5 .sec2:first-child,
+        .print.a5 .sec2:last-child{
+            width: 4.9cm;
+        }
+        .receipt{
+            border-collapse: collapse;
+            width: 100%;
+            font-size: 12px;
+        }
+        .receipt thead td{
+            font-family: "Siemreap","KhmerOS", sans-serif;
+            line-height: 16px;
+            text-align: center;
+        }
+        .receipt td, .tb05 th{
+            border: 1px solid #bab6b6;
+            padding: 3px 5px;
+        }
+        .receipt tbody td{
+            font-family: "Siemreap","KhmerOS", sans-serif;
+        }
+        .receipt tbody td.no, .receipt thead td.no{
+            width: 20px;
+            text-align: center;
+        }
+        .receipt td:nth-child(3){width: 40px;}
+        .receipt td:nth-child(4){width: 70px;}
+        .receipt td:nth-child(5){width: 80px;}
+        .receipt tbody td:nth-child(3){
+            text-align: center;
+        }
+        .receipt tbody td:nth-child(4),
+        .receipt tbody td:nth-child(5){
+            text-align: right;
+        }
+        .receipt tfoot{
+            font-family: "Siemreap","KhmerOS", sans-serif;
+        }
+        .receipt tfoot tr:first-child td:first-child{
+            border: none !important;
+            vertical-align: top;
+            padding: 0;
+        }
+        .receipt tfoot .foott{
+            border: none !important;
+            text-align: right;
+        }
+        .print.a5 .minifoot{
+            padding-bottom: 1cm;
+            padding-left: 1cm;
+        }
+        .p-r{position: relative;}
+        .p-a{position: absolute;}
+        .t-0{top: 0}.r-0{right: 0}.b-0{bottom: 0}.l-0{left: 0}
+        .t_a_c{text-align: center;}
+        .t_a_l{text-align: left;}
+        .t_a_r{text-align: right;}
+        .fs_24{font-size: 24px;}
+        .fs_20{font-size: 20px;}
+        .fs_18{font-size: 18px;}
+        .fs_12{font-size: 12px;}
+        .fw_b{font-weight: bold}
+        .ds_f{display: flex;display: -moz-flex; display: -webkit-flex; display: -ms-flex;}
+        .lh_15{line-height: 15px;}
+        .lh_18{line-height: 18px;}
+        .lh_20{line-height: 20px;}
+        .pl_20{padding-left: 20px;}
+        .fm-popp{}
+        .fm-bokor{}
+        .fm-ubt5{}
+        .fm-smreap{}
+        .fm-moul{}
+        .pt_3{padding-top: 3px;}
+        .pt_5{padding-top: 5px;}
+        .pt_10{padding-top: 10px;}
+        .w_30{width: 30px;}
+        .w_40{width: 40px;}
+        .w_45{width: 45px;}
+        .wp_100{width: 100%}
+        .pr_8{padding-right: 8px;}
+        .fx_4{-ms-flex:0 0 33.333333%;flex:0 0 33.333333%;max-width:33.333333%}
+        .f_row{flex-flow: row wrap}
+        .f_col{flex-flow: column}
+    </style>
+</head>
+<body style="background-color: gainsboro">
+<div class="print a5">
+    <div class="margin p-r">
+        <div class="fm-moul t_a_c">
+            <div class="fs_24 color pen">លី ថៃ</div>
+        </div>
+        <div class="ds_f color pen f_row">
+            <div class="mleft f_col">
+                <div class="fm-bokor fs_12 lh_18">
+                    <div>ផ្ទះលេខ 75-79</div>
+                    <div>សង្កាត់ទួលស្វាយព្រៃ២ </div>
+                    <div>ខណ្ណចំការមន ភ្នំពេញ</div>
+                </div>
+            </div>
+            <div class="mmiddle">
+                <div class="fm-bokor fs_12 lh_18">
+                    <div>លក់បោះដុំ ខោទ្រនាប់ អាវទ្រនាប់ ឈុតកីឡា កន្សែង</div>
+                    <div>សារុង អាវសិស្សសាលា ស្រោមដៃ ស្រោមជើង</div>
+                    <div>និងសម្លៀកបំពាក់ពីទារកដល់មនុស្សចាស់គ្រប់ប្រភេទ</div>
+                </div>
+            </div>
+            <div class="mright fs_12">
+                <div class="pl_20 fm-popp">
+                    <div class="ds_f">
+                        <div class="w_30">TEL :</div>
+                        <div>
+                            <div>010 563 093</div>
+                            <div>092 235 043</div>
+                            <div>096 235 0431</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ds_f pt_5">
+            <div class="sec2 fm-smreap fs_12 p-r color pen">
+                <div class="p-a b-0 l-0">
+                    <div class="ds_f">
+                        <div class="w_45">អតិថិជន</div>
+                        <div>: លី ឯល</div>
+                    </div>
+                    <div class="ds_f">
+                        <div class="w_45">TEL</div>
+                        <div>: 010 563 093</div>
+                    </div>
+                </div>
+            </div>
+            <div class="sec2 t_a_c lh_20 color pen">
+                <div class="fm-moul pt_3">វិក័យប័ត្រ</div>
+                <div class="fm-ubt5">INVOICE</div>
+            </div>
+            <div class="sec2 p-r">
+                <div class="t_a_r p-a b-0 r-0 color red">
+                    <span class="fw_b fs_20">N<sup><u>o</u></sup> : </span>
+                    <span class="fs_18 fw_b">2021-001</span>
+                </div>
+            </div>
+        </div>
+        <div class="pt_5">
+            <table class="receipt">
+                <thead>
+                <tr>
+                    <td class="no">
+                        <div>ល.រ</div>
+                        <div>N<sup><u>o</u></sup></div>
+                    </td>
+                    <td>
+                        <div>ឈ្មោះទំនិញ</div>
+                        <div>Description</div>
+                    </td>
+                    <td>
+                        <div>ចំនួន</div>
+                        <div>Quantity</div>
+                    </td>
+                    <td>
+                        <div>តម្លៃរាយ</div>
+                        <div>Unit Price</div>
+                    </td>
+                    <td>
+                        <div>តម្លៃសរុប</div>
+                        <div>Amount</div>
+                    </td>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <td class="no">1</td>
+                    <td>អាវកាក់</td>
+                    <td>200</td>
+                    <td>$50,000.00</td>
+                    <td>$20,000,00.00</td>
+                </tr>
+                <tr>
+                    <td class="no">1</td>
+                    <td>អាវកាក់</td>
+                    <td>3</td>
+                    <td>$50,000.00</td>
+                    <td>$20,000,00.00</td>
+                </tr>
+                <tr>
+                    <td class="no">1</td>
+                    <td>អាវកាក់</td>
+                    <td>5</td>
+                    <td>$50,000.00</td>
+                    <td>$20,000,00.00</td>
+                </tr>
+                <tr>
+                    <td class="no">1</td>
+                    <td>អាវកាក់</td>
+                    <td>7</td>
+                    <td>$50,000.00</td>
+                    <td>$20,000,00.00</td>
+                </tr>
+                </tbody>
+
+                <tfoot>
+                <tr>
+                    <td colspan="3" rowspan="3">
+                        <div class="ds_f lh_15 pt_10">
+                            <div class="fx_4">
+                                <div>អតិថិជន</div>
+                                <div>Customer</div>
+                            </div>
+                            <div class="fx_4">
+                                <div>អ្នគិតលុយ</div>
+                                <div>Cashier</div>
+                            </div>
+                            <div class="fx_4">
+                                <div>អ្នកលក់</div>
+                                <div>Seller</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="foott">សរុប/Total</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="foott">ទូទាត់/Paid</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="foott">នៅខ្វះ/Due</td>
+                    <td></td>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
+
+        <div class="p-a b-0 l-0 fm-smreap minifoot fs_12">
+            <div class="ds_f">
+                <div class="w_40">
+                    <div class="pr_8 pt_3">
+                        <img class="wp_100" src="" alt="">
+                    </div>
+                </div>
+                <div class="lh_20">
+                    <div>001 300 480 (លុយដុល្លា $)</div>
+                    <div>001 760 147 (លុយរៀល ៛)</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
